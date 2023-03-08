e09fe27ccee215f436e1845d887aa94481c33d01
Liam Ballantyne <ljjb97@gmail.com>
2023-03-07 23:37:05 -0500

    Fixed test assertions

--- src/test/java/com/example/SurveyControllerTest.java
+++ src/test/java/com/example/SurveyControllerTest.java
@@ -23,56 +23,18 @@ public class SurveyControllerTest {
     private MockMvc mockMvc;
 
 
-    String createPage = """
-                                                              <!DOCTYPE html>
-                                                              <html>
-                                                              <head>
-                                                                <title>Create Survey</title>
-                                                              </head>
-                                                              <body>
-                                                              <h1>Create Survey</h1>
-                                                              <form method="post" action="/survey">
-                                                                <label for="title">Title:</label>
-                                                                <input type="text" id="title" name="title" required><br><br>
-                                                                <label for="description">Description:</label>
-                                                                <input type="text" id="description" name="description" required><br><br>
-                                                                <input type="submit" value="Save Survey">
-                                                              </form>
-                                                              </body>
-                                                              </html>
-                                                              """.replaceAll("\n", "\r\n");
-
-    String homePage = """
-            <!DOCTYPE HTML>
-            <html xmlns="http://www.w3.org/1999/xhtml">
-            <head>
-                <title>Getting Started: Serving Web Content</title>
-                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-                <link rel="stylesheet" href="/css/signin.css"/>
-            </head>
-            <body>
-            <p> Create a Survey <a href="/createsurvey">here</a></p>
-            <p> View All Surveys <a href="/viewsurveys">here</a></p>
-            <p> View Specific Survey <a href="/selectsurvey">here</a></p>
-            <p> View Questions (Not Implemented) <a href="/viewquestions">here</a></p>
-            <p> View Results (Not Implemented) <a href="/removesurvey">here</a></p>
-            <p> View H2 Console <a href="http://localhost:8080/h2-console">here</a> </p>
-            </body>
-            </html>
-            """.replaceAll("\n", "\r\n");
-
     @Test
     public void testCreate() throws Exception{
 
         //Get the surveys questions
         mockMvc.perform(get("/createsurvey", ""))
-                .andDo(print()).andExpect(content().string(createPage));
+                .andDo(print()).andExpect(MockMvcResultMatchers.view().name("createsurvey"));
         mockMvc.perform(post("/survey", "")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("title", "test")
                         .param("description", "test"))
                 .andDo(print())
-                .andExpect(content().string(homePage))
+                .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andExpect(status().is2xxSuccessful());
 
     }
@@ -87,7 +49,7 @@ public class SurveyControllerTest {
                         .param("title", "test")
                         .param("description", "test"))
                 .andDo(print())
-                .andExpect(content().string(homePage))
+                .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andExpect(status().is2xxSuccessful());
         mockMvc.perform(get("/viewsurveys", ""))
                 .andDo(print())
