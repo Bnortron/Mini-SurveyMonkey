5f764b8e947e47fd8fdb3669821ac2de9ee7a4d7
Bnortron <72238857+bnortron@users.noreply.github.com>
2023-03-26 14:44:07 -0400

    Attempt at setting up ObjectDB: - Added config and adapter classes for the application - Updated dependencies in pom.xml
    Signed-off-by: Brady Norton <bnort.11@gmail.com>

--- src/main/java/com/example/QuestionRepository.java
+++ src/main/java/com/example/QuestionRepository.java
@@ -1,9 +1,11 @@
 package com.example;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface QuestionRepository extends CrudRepository<SurveyQuestion, Long> {
     List<SurveyQuestion> findBySurvey(Survey survey);
 }
