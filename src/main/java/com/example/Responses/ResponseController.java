cd49d26ba088f4435f775fe114973dfd67945aba
Bnortron <72238857+bnortron@users.noreply.github.com>
2023-04-10 15:30:10 -0400

    Viewing Responses Functional: - Added viewQuestionResponses() method to ResponseController.java class for viewing all the responses to a specific question - Added corresponding viewquestionresponses.html page that lists the table of responses for a selected question
    Signed-off-by: Brady Norton <bnort.11@gmail.com>

--- src/main/java/com/example/Responses/ResponseController.java
+++ src/main/java/com/example/Responses/ResponseController.java
@@ -6,9 +6,7 @@ import com.example.Surveys.SurveyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,8 +78,24 @@ public class ResponseController {
         return "redirect:/survey?selectedSurvey=" + surveyid;
     }
 
+    @GetMapping("/surveys/{id}/questions/{questionId}/responses")
+    public String viewQuestionResponses(@PathVariable("id") Long surveyId,
+                                        @PathVariable("questionId") Long questionId,
+                                        Model model) {
+        // Retrieve the survey and question data based on the surveyId and questionId
+        Optional<Survey> survey = surveyRepository.findById(surveyId);
+        Optional<Question> question = questionRepository.findById(questionId);
 
+        // Retrieve the responses for the given question
+        List<Response> responses = responseRepository.findByQuestion(question.get());
 
+        // Add the survey, question, and response data to the model for use in the Thymeleaf template
+        model.addAttribute("survey", survey.get());
+        model.addAttribute("question", question.get());
+        model.addAttribute("responses", responses);
+
+        return "viewquestionresponses";
+    }
 }
 
 
--- src/main/java/com/example/Surveys/SurveyController.java
+++ src/main/java/com/example/Surveys/SurveyController.java
@@ -1,6 +1,7 @@
 package com.example.Surveys;
 
 import com.example.Questions.*;
+import com.example.Responses.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -258,5 +259,4 @@ public class SurveyController {
 
         return "viewresponses";
     }
-
 }
